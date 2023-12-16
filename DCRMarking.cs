9d2442b0fb0b069b5d5d1f988ee1d132ed1330f2
Pepijn Bakker <bakker.pepijn@gmail.com>
2023-12-16 02:38:52 +0100

    added dummy functions and builder functions

--- DCRMarking.cs
+++ DCRMarking.cs
@@ -1,7 +1,7 @@
 namespace Core;
 
 public struct DCRMarking<T> : ICloneable where T : notnull { 
-    public HashSet<T> executed = [], included = [], pending = [];
+    public readonly HashSet<T> executed = [], included = [], pending = [];
 
     public DCRMarking(HashSet<T> executed, HashSet<T> included, HashSet<T> pending) {
         this.executed = executed;
