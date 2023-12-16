af033631c1fb2823e657551f7a40cb78f10ff9ee
Pepijn Bakker <bakker.pepijn@gmail.com>
2023-12-16 17:24:41 +0100

    fixed builder functions

--- Core/DCRGraph.cs
+++ Core/DCRGraph.cs
@@ -40,7 +40,14 @@ public class DCRGraph<T> where T : IEvent {
 
     #region Builder functions
 
-    public void AddEvent(T e) => _events.Add(e);
+    public void AddEvent(T e) {
+        _events.Add(e);
+        _conditions[e] = [];
+        _milestones[e] = [];
+        _responses[e] = [];
+        _excludes[e] = [];
+        _includes[e] = [];
+    }
 
     // first two are reversed! condition[to] = from
     public void AddCondition (T from, T to) => _conditions[to].Add(from);
@@ -57,7 +64,20 @@ public class DCRGraph<T> where T : IEvent {
     #endregion
 
     public bool IsEnabled(T e) {
-        throw new NotImplementedException();
+        // if e is not included, it is not enabled
+        if (!_marking.Included.Contains(e)) 
+            return false;
+
+        // if e has unexecuted conditions, it is not enabled
+        if (_conditions[e].Any(c => !_marking.Executed.Contains(c))) 
+            return false;
+
+        // if e has pending milestones, it is not enabled
+        if (_milestones[e].Any(m => _marking.Pending.Contains(m))) 
+            return false;
+        
+        // otherwise, it is enabled
+        return true;
     }
 
     public void Execute(T e) {
