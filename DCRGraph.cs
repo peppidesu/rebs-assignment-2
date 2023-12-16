530a933c7f2f9b602714ea178f4214c96a4f90d3
Pepijn Bakker <bakker.pepijn@gmail.com>
2023-12-16 02:02:31 +0100

    formatting

--- DCRGraph.cs
+++ DCRGraph.cs
@@ -5,16 +5,25 @@ namespace Core;
 /// </summary>
 /// <typeparam name="T"></typeparam>
 public class DCRGraph<T> where T : notnull { 
+
+    // nodes
     private HashSet<T> _events = []; 
-    private Dictionary<T, HashSet<T>> _conditions = []; 
-    private Dictionary<T, HashSet<T>> _milestones = []; 
-    private Dictionary<T, HashSet<T>> _responses = []; 
-    private Dictionary<T, HashSet<T>> _excludes = []; 
-    private Dictionary<T, HashSet<T>> _includes = [];
+
+    // edges
+    private Dictionary<T, HashSet<T>> _conditions = [],
+                                      _milestones = [],
+                                      _responses = [],
+                                      _excludes = [],
+                                      _includes = [];
     
     private DCRMarking<T> _marking = new DCRMarking<T>([], [], []);
 
-    public DCRGraph(HashSet<T> events, Dictionary<T, HashSet<T>> conditions, Dictionary<T, HashSet<T>> milestones, Dictionary<T, HashSet<T>> responses, Dictionary<T, HashSet<T>> excludes, Dictionary<T, HashSet<T>> includes) {
+    public DCRGraph(HashSet<T> events, 
+                    Dictionary<T, HashSet<T>> conditions, 
+                    Dictionary<T, HashSet<T>> milestones, 
+                    Dictionary<T, HashSet<T>> responses, 
+                    Dictionary<T, HashSet<T>> excludes, 
+                    Dictionary<T, HashSet<T>> includes) {
         _events = events;
         _conditions = conditions;
         _milestones = milestones;
