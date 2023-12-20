6598e21d21cae8fe5da36db38a19269da9064fc0
peppidesu <bakker.pepijn@gmail.com>
2023-12-20 15:08:10 +0100

    versioning

---
+++ App/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+
+using System.Reflection;
+
+[assembly: AssemblyCompany("Max Kowalchuk, Pepijn Bakker, Hjalte Tromborg")]
+[assembly: AssemblyCopyright("Distributed under the Apache-2.0 Licence")]
+[assembly: AssemblyTitle("DCRConformanceChecker")]
+[assembly: AssemblyVersion("1.0.*")]
